308da05c229d6ba38077e32cfb18ad1160bc160b
Fiston <fistonhn@gmail.com>
2025-06-19 01:35:35 +0200

    DsEstimation add frontend build folder

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -51,12 +51,33 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "postgres",
-    password: "F@123456",
-    database: "dsestimation",
-    host: "127.0.0.1",          // ✅ add this
-    port: 5432,                 // ✅ add this
-    dialect: "postgres",
+    // username: "postgres",
+    // password: "F@123456",       // npg_cGnrms63tKYH  this is the password for the postgres user production
+    // database: "dsestimation",
+    // host: "127.0.0.1",          // ✅ add this
+    // port: 5432,                 // ✅ add this
+    // dialect: "postgres",
+    // logging: false,
+    // pool: {
+    //   max: 20,
+    //   min: 0,
+    //   acquire: 60000,
+    //   idle: 10000
+    // }
+
+    
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
     pool: {
       max: 20,
@@ -66,13 +87,33 @@ module.exports = {
     }
   },
   test: {
-    username: "postgres",
-    password: "F@123456",
-    database: "dsestimationtest",
-    host: "127.0.0.1",
-    port: 5432,
-    dialect: "postgres",
+    // username: "postgres",
+    // password: "F@123456",
+    // database: "dsestimationtest",
+    // host: "127.0.0.1",
+    // port: 5432,
+    // dialect: "postgres",
+    // logging: false,
+
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
+    pool: {
+      max: 20,
+      min: 0,
+      acquire: 60000,
+      idle: 10000
+    }
   },
   production: {
     username: process.env.POSTGRES_USER,
